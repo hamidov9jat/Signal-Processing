ddc0cfa3f814198681008b0aa8277770b2c1308c
hamidov9jat <105560721+hamidov9jat@users.noreply.github.com>
2023-01-31 22:35:34 +0400

    Delete report project/.ipynb_checkpoints directory

--- report project/.ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from scipy.io import wavfile
-import scipy.signal
-import numpy as np
-import matplotlib.pyplot as plt
-# import librosa
-
-wav_loc = "test.wav"
-rate, data = wavfile.read(wav_loc)
-data = data / 32768
